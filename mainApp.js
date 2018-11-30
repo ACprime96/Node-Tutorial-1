badc6a18a2e21d73b7497c53a91deb0a94e45b7d
AC <adihyachndrt@gmail.com>
2018-11-30 15:00:25 +0530

    Basics

---
+++ mainApp.js
@@ -0,0 +1,7 @@
+const http = require('http');
+const fs = require('fs');
+const json = require('./data.json');
+
+const server = http.createServer(function(req, res){
+  // TODO:
+}).listen(8080, ()=>{console.log('Server running on 8080');}); 
